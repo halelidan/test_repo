189a890840ec25f395c17b3966cd2d2a93f6c94d
halelidan <132199195+halelidan@users.noreply.github.com>
2025-02-17 16:45:01 +0200

    Update ad_group_ad_asset_view.sql

--- text_assets_performance_dash/google_ads_queries/ad_group_ad_asset_view.sql
+++ text_assets_performance_dash/google_ads_queries/ad_group_ad_asset_view.sql
@@ -11,6 +11,19 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
+-- Copyright 2022 Google LLC
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     https://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
 SELECT
     segments.date AS date,
     customer.id AS customer_id,
@@ -19,7 +32,7 @@ SELECT
     campaign.name AS campaign_name,
     campaign.status AS campaign_status,
     campaign.labels AS campaign_label,
-    ad_group.id as ad_group_id,
+    ad_group.id AS ad_group_id,
     ad_group.name AS ad_group_name,
     ad_group.labels AS ad_group_label,
     ad_group.status AS ad_group_status,
@@ -37,5 +50,9 @@ SELECT
     metrics.ctr AS ctr,
     metrics.impressions AS impressions
 FROM ad_group_ad_asset_view
-WHERE segments.date >= '{start_date}'
+ WHERE campaign.advertising_channel_type = 'SEARCH'
+ AND campaign.status = 'ENABLED'
+ AND ad_group.status = 'ENABLED'
+ AND ad_group_ad.status = 'ENABLED'
+ AND segments.date >= '{start_date}'
  AND segments.date <= '{end_date}'
